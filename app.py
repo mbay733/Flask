5b02330ada239752776ea4a81c509d27ec7916c0
Murat Bayramukov <bay733@mail.ru>
2024-09-10 22:01:58 +0300

    Add about URL

--- app.py
+++ app.py
@@ -3,6 +3,19 @@ from flask import Flask
 app = Flask(__name__)
 
 
+
+about_me = {
+"name": "Мурат",
+"surname": "Байрамуков",
+"email": "Bay733@mail.ru"
+}
+
+@app.route("/about")
+def about():
+    return about_me
+
+
+
 @app.route("/")   #первый URL который мы обробатываем
 def hello_world():   #Это функц-обработчик будет вызван при запросе УРЛ
     return "Hello, World!"
